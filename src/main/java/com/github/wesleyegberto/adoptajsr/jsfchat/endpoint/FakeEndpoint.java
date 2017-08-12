b644247a86a9dde8ac29dec29220bef0bbff6f16
Wesley Egberto <wesleyegberto@gmail.com>
2017-08-12 16:56:09 -0300

    Added websocket clock

--- src/main/java/com/github/wesleyegberto/adoptajsr/jsfchat/endpoint/FakeEndpoint.java
+++ src/main/java/com/github/wesleyegberto/adoptajsr/jsfchat/endpoint/FakeEndpoint.java
@@ -5,10 +5,8 @@ import javax.websocket.EndpointConfig;
 import javax.websocket.Session;
 
 public class FakeEndpoint extends Endpoint {
-
 	@Override
 	public void onOpen(Session session, EndpointConfig config) {
 		// https://java.net/jira/browse/WEBSOCKET_SPEC-240
-		System.out.println("[FakeEndpoint] onOpen");
 	}
 }
--- src/main/java/com/github/wesleyegberto/adoptajsr/jsfchat/service/ChatRoomObserver.java
+++ src/main/java/com/github/wesleyegberto/adoptajsr/jsfchat/service/ChatRoomObserver.java
@@ -2,12 +2,14 @@ package com.github.wesleyegberto.adoptajsr.jsfchat.service;
 
 import java.util.logging.Logger;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
 import javax.faces.event.WebsocketEvent;
 import javax.faces.event.WebsocketEvent.Closed;
 import javax.faces.event.WebsocketEvent.Opened;
 import javax.inject.Inject;
 
+@ApplicationScoped
 public class ChatRoomObserver {
 	@Inject
 	private Logger LOG;
@@ -18,6 +20,8 @@ public class ChatRoomObserver {
 	}
 
 	public void onClose(@Observes @Closed WebsocketEvent event) {
+        String channel = event.getChannel();        
+        LOG.info("Channel " + channel + " was successfully closed!");
 		LOG.info("Closed connection " + event.getChannel() + " from " + event.getUser() + " with code "
 				+ event.getCloseCode());
 	}
