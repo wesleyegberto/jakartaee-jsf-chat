526cc939315df39e10f17061daead6329b78e25a
Wesley Egberto <wesleyegberto@gmail.com>
2022-11-15 16:53:59 -0300

    renamed Dockerfile

--- buildAndRunTomEE.sh
+++ buildAndRunTomEE.sh
@@ -3,5 +3,5 @@ set -e exit
 
 mvn clean package
 docker rm -f jsf-chat-tomee || true
-docker build -t wesleyegberto/jsf-chat-tomee -f tomee.Dockerfile .
+docker build -t wesleyegberto/jsf-chat-tomee -f Dockerfile.tomee .
 docker run -it -p 8080:8080 --name jsf-chat-tomee wesleyegberto/jsf-chat-tomee
